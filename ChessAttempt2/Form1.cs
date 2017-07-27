5c636f04547e90178926bc8394508fc0b922f4ad
Julian Meyn <Ic3Venom@gmail.com>
2017-07-27 14:42:15 -0700

    confuzzling

--- ChessAttempt2/Form1.cs
+++ ChessAttempt2/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Collections.Generic;
-using System.Reflection;
 
 namespace ChessAttempt2
 {
@@ -9,7 +8,7 @@ namespace ChessAttempt2
     {
         List<Piece> pieces = new List<Piece>();
         Label lastPiece = null;
-        bool side = true, gameStart = false;
+        bool side = true, gameStart = false, moved = false;
         int p1time, p2time;
 
 
@@ -19,44 +18,44 @@ namespace ChessAttempt2
             Piece.InitCustomLabelFont();
             InitializeBoard();
 
-            message.Text = "Press anywhere to begin the game!";
+            message.Text = "Click here to begin the game";
         }
 
         private void InitializeBoard()
         {
             //Wall of text, but it's better than looping through null labels
-            pieces.Add(new Piece("Rook",   true,  row8_1));
-            pieces.Add(new Piece("Knight", true,  row8_2));
-            pieces.Add(new Piece("Bishop", true,  row8_3));
-            pieces.Add(new Piece("Queen",  true,  row8_4));
-            pieces.Add(new Piece("King",   true,  row8_5));
-            pieces.Add(new Piece("Bishop", true,  row8_6));
-            pieces.Add(new Piece("Knight", true,  row8_7));
-            pieces.Add(new Piece("Rook",   true,  row8_8));
-            pieces.Add(new Piece("Rook",   false, row1_1));
-            pieces.Add(new Piece("Knight", false, row1_2));
-            pieces.Add(new Piece("Bishop", false, row1_3));
-            pieces.Add(new Piece("Queen",  false, row1_4));
-            pieces.Add(new Piece("King",   false, row1_5));
-            pieces.Add(new Piece("Bishop", false, row1_6));
-            pieces.Add(new Piece("Knight", false, row1_7));
-            pieces.Add(new Piece("Rook",   false, row1_8));
-            pieces.Add(new Piece("Pawn", true,  row7_1));
-            pieces.Add(new Piece("Pawn", true,  row7_2));
-            pieces.Add(new Piece("Pawn", true,  row7_3));
-            pieces.Add(new Piece("Pawn", true,  row7_4));
-            pieces.Add(new Piece("Pawn", true,  row7_5));
-            pieces.Add(new Piece("Pawn", true,  row7_6));
-            pieces.Add(new Piece("Pawn", true,  row7_7));
-            pieces.Add(new Piece("Pawn", true,  row7_8));
-            pieces.Add(new Piece("Pawn", false, row2_1));
-            pieces.Add(new Piece("Pawn", false, row2_2));
-            pieces.Add(new Piece("Pawn", false, row2_3));
-            pieces.Add(new Piece("Pawn", false, row2_4));
-            pieces.Add(new Piece("Pawn", false, row2_5));
-            pieces.Add(new Piece("Pawn", false, row2_6));
-            pieces.Add(new Piece("Pawn", false, row2_7));
-            pieces.Add(new Piece("Pawn", false, row2_8));
+            pieces.Add(new Piece(Piece.pieceNames[3], true,  row8_1));
+            pieces.Add(new Piece(Piece.pieceNames[1], true,  row8_2));
+            pieces.Add(new Piece(Piece.pieceNames[2], true,  row8_3));
+            pieces.Add(new Piece(Piece.pieceNames[4], true,  row8_4));
+            pieces.Add(new Piece(Piece.pieceNames[5], true,  row8_5));
+            pieces.Add(new Piece(Piece.pieceNames[2], true,  row8_6));
+            pieces.Add(new Piece(Piece.pieceNames[1], true,  row8_7));
+            pieces.Add(new Piece(Piece.pieceNames[3], true,  row8_8));
+            pieces.Add(new Piece(Piece.pieceNames[3], false, row1_1));
+            pieces.Add(new Piece(Piece.pieceNames[1], false, row1_2));
+            pieces.Add(new Piece(Piece.pieceNames[2], false, row1_3));
+            pieces.Add(new Piece(Piece.pieceNames[4], false, row1_4));
+            pieces.Add(new Piece(Piece.pieceNames[5], false, row1_5));
+            pieces.Add(new Piece(Piece.pieceNames[2], false, row1_6));
+            pieces.Add(new Piece(Piece.pieceNames[1], false, row1_7));
+            pieces.Add(new Piece(Piece.pieceNames[3], false, row1_8));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_8));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_1));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_2));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_3));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_4));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_5));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_6));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_7));
+            pieces.Add(new Piece(Piece.pieceNames[0], true,  row7_8));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_1));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_2));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_3));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_4));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_5));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_6));
+            pieces.Add(new Piece(Piece.pieceNames[0], false, row2_7));
         }
 
         private void Clocks()
@@ -80,6 +79,47 @@ namespace ChessAttempt2
 
         }
 
+        private Piece findPiece(Label label)
+        {
+            foreach (Piece piece in pieces)
+            {
+                if (label == piece.GetLabel())
+                {
+                    return piece;
+                }
+            }
+            return new Piece("None", true, label);
+        }
+
+        private void resetColor(Label piece)
+        {
+            if (piece != null)
+            {
+                if ((int)(piece.Name[3] - '0') % 2 == 0)
+                {
+                    if ((int)(piece.Name[5] - '0') % 2 == 0)
+                    {
+                        piece.BackColor = System.Drawing.Color.White;
+                    }
+                    else
+                    {
+                        piece.BackColor = System.Drawing.Color.Gray;
+                    }
+                }
+                else
+                {
+                    if ((int)(piece.Name[5] - '0') % 2 == 0)
+                    {
+                        piece.BackColor = System.Drawing.Color.Gray;
+                    }
+                    else
+                    {
+                        piece.BackColor = System.Drawing.Color.White;
+                    }
+                }
+            }
+        }
+
         private void game_Start(object sender, EventArgs e)
         {
             message.Click -= game_Start;
@@ -100,43 +140,50 @@ namespace ChessAttempt2
             {
                 resetColor(lastPiece);
 
-                Label label = sender as Label;
+                Label thisPiece = sender as Label;
 
-                label.BackColor = System.Drawing.Color.LightGreen;
+                if(!moved)
+                {
+                    thisPiece.BackColor = System.Drawing.Color.LightGreen;
+                    lastPiece = thisPiece;
+                    moved = !moved;
+                    return;
+                }
 
-                if (Piece.ValidateMove(lastPiece, label))
+                Piece p1 = findPiece(thisPiece);
+                Piece p2 = findPiece(lastPiece);
+                
+                if (Piece.ValidateMove(p1, p2))
+                {
+                    p2.MovePiece(p1);
+                    pieces.Remove(p2);
+                    resetColor(thisPiece);
+
+                    lastPiece = null;
+                    moved = false;
                     Clocks();
 
-                lastPiece = label;
-            }
-        }
-
-        private void resetColor(Label lastPiece)
-        {
-            if (lastPiece != null)
-            {
-                if ((int)(lastPiece.Name[3] - '0') % 2 == 0)
-                {
-                    if ((int)(lastPiece.Name[5] - '0') % 2 == 0)
-                    {
-                        lastPiece.BackColor = System.Drawing.Color.White;
-                    }
-                    else
-                    {
-                        lastPiece.BackColor = System.Drawing.Color.Gray;
-                    }
+                    return;
                 }
                 else
                 {
-                    if ((int)(lastPiece.Name[5] - '0') % 2 == 0)
-                    {
-                        lastPiece.BackColor = System.Drawing.Color.Gray;
-                    }
-                    else
-                    {
-                        lastPiece.BackColor = System.Drawing.Color.White;
-                    }
+                    lastPiece = thisPiece;
+                    resetColor(thisPiece);
                 }
+                /*if (p2.GetSide() == side)
+                {
+                    if (p2 != null && Piece.ValidateMove(p1, p2))
+                    {
+                        p2.MovePiece(thisPiece);
+                        thisPiece.Dispose();
+                        pieces.Remove(findPiece(lastPiece));
+                        resetColor(thisPiece);
+                        lastPiece = null;
+                        Clocks();
+                        return;
+                    }
+                    lastPiece = thisPiece;
+                }*/
             }
         }
 
@@ -144,14 +191,12 @@ namespace ChessAttempt2
         {
             p1time++;
             player1Time.Text = "P1: " + (TimeSpan.FromSeconds(p1time).ToString(@"hh\:mm\:ss"));
-            
         }
 
         private void player2_Tick(object sender, EventArgs e)
         {
             p2time++;
             player2Time.Text = "P2: " + (TimeSpan.FromSeconds(p2time).ToString(@"hh\:mm\:ss"));
-            
         }
     }
 }
--- ChessAttempt2/piece.cs
+++ ChessAttempt2/piece.cs
@@ -9,17 +9,19 @@ namespace ChessAttempt2
     {
         private string name;
         private bool side;
+        private Label label;
 
         //font used for chess pieces
-        static PrivateFontCollection pfc = new PrivateFontCollection();
-        private static char[] whitePieces = { 'p', 'n', 'b', 'r', 'q', 'k' };
-        private static char[] blackPieces = { 'o', 'm', 'v', 't', 'w', 'l' };
-        public static List<string> pieceNames = new List<string>() { "Pawn", "Knight", "Bishop", "Rook", "Queen", "King" };
+        private static PrivateFontCollection pfc = new PrivateFontCollection();
+        private static char[] whitePieces = { 'p', 'n', 'b', 'r', 'q', 'k', ' '};
+        private static char[] blackPieces = { 'o', 'm', 'v', 't', 'w', 'l'};
+        public static List<string> pieceNames = new List<string>() { "Pawn", "Knight", "Bishop", "Rook", "Queen", "King", "None"};
 
         public Piece(string name, bool side, Label start)
         {
             this.name = name;
             this.side = side;
+            this.label = start;
 
             if (side)
             {
@@ -45,39 +47,86 @@ namespace ChessAttempt2
             pfc.AddMemoryFont(data, fontLength);
         }
 
-        public static bool ValidateMove(Label start, Label end)
+        public void MovePiece(Piece newPiece)
+        {
+            newPiece.label.Font = new System.Drawing.Font(pfc.Families[0], 72);
+
+            newPiece.label.Text = this.label.Text;
+            this.label.Text = null;
+            this.label = newPiece.label;
+        }
+
+        public static bool ValidateMove(Piece p1, Piece p2)
+        {
+            //If both pieces are empty
+            if (p1 == null && p2 == null)
+                return false;
+            //If the first piece is empty
+            if (p2.label.Text == " ")
+                return false;
+            //If the second piece is empty
+            else if (p1.label.Text == " ")
+                return true;
+            //If both pieces are of the same color
+            else if ((p1.name == "None" && p2.side == true) || (p1.side == true && p2.side == true))
+                return false;
+            else
+                return false;
+
+        }
+
+        private bool Pawn(Label end)
         {
             return true;
         }
 
-        private bool Pawn(Label start, Label end)
+        private bool Knight(Label end)
         {
             return true;
         }
 
-        private bool Knight(Label start, Label end)
+        private bool Bishop(Label end)
         {
             return true;
         }
 
-        private bool Bishop(Label start, Label end)
+        private bool Rook(Label end)
         {
             return true;
         }
 
-        private bool Rook(Label start, Label end)
+        private bool Queen(Label end)
         {
             return true;
         }
 
-        private bool Queen(Label start, Label end)
+        private bool King(Label end)
         {
             return true;
         }
 
-        private bool King(Label start, Label end)
+        public string GetName( )
         {
-            return true;
+            return name;
+        }
+        public bool GetSide( )
+        {
+            return side;
+        }
+        public Label GetLabel( )
+        {
+            return label;
+        }
+        public void SetName(string name)
+        {
+            if (side)
+            {
+                label.Text = whitePieces[pieceNames.IndexOf(name)].ToString();
+            }
+            else
+            {
+                label.Text = blackPieces[pieceNames.IndexOf(name)].ToString();
+            }
         }
     }
 }
